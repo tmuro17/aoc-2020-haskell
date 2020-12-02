5d48d2e95aa50e883c553514bae38d3ff3e2b5b2
Tanner Muro <tmuro17@gmail.com>
2020-12-02 01:26:53 -0500

    Still need to add tests for this.  Got caught up with trying to make parsing work, this ended up being easier.

--- day02/src/day02.hs
+++ day02/src/day02.hs
@@ -1,34 +1,28 @@
-import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Data.Void (Void)
 
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
-import qualified Control.Applicative as CA
-
+import qualified Data.Map as M
 
 main :: IO ()
 main = do
-        text <- TIO.readFile "data/input1.txt"
-        let inputTxt = successfulParse text
-        print inputTxt
+        text <- TIO.readFile "data/input2.txt"
+        lns <- pure $ T.lines text
+        print $ length $ filter (\[range, tc, pass] -> isValid (rangeToTuple range) (targetChar tc) (T.unpack pass)) $ map (T.words) lns
+        print $ length $ filter (\[range, tc, pass] -> isValid' (rangeToTuple range) (targetChar tc) (T.unpack pass)) $ map (T.words) lns
 
--- Parse the input file
-type Parser = Parsec Void Text
 
-sc :: Parser ()
-sc = L.space (skipSome spaceChar) CA.empty CA.empty
--- sc = L.space (skipSome (char ' ')) CA.empty CA.empty
+rangeToTuple :: T.Text -> (Integer, Integer)
+rangeToTuple txt = (\[l, u] -> (read $ T.unpack l, read $ T.unpack u)) $ T.splitOn (T.pack "-") txt
 
-lexeme  = L.lexeme sc
-integer = lexeme L.decimal
--- signedInteger = L.signed sc integer
+targetChar :: T.Text -> Char
+targetChar = head . T.unpack
 
-moduleP = many integer
+isValid :: (Integer, Integer) -> Char -> String -> Bool
+isValid (lBnd,uBnd) target pass = case M.lookup target $ M.fromListWith (+) [(c, 1) | c <- pass] of
+    Just x -> (x >= lBnd) && (x <= uBnd)
+    Nothing -> False
 
-successfulParse :: Text -> [Integer]
-successfulParse input =
-        case parse moduleP "input" input of
-                Left  _err -> [] -- TIO.putStr $ T.pack $ parseErrorPretty err
-                Right nums -> nums
+isValid' :: (Integer, Integer) -> Char -> String -> Bool
+isValid' (pos1, pos2) target pass = ((pos1 `elem` psns) && (not $ pos2 `elem` psns)) || ((not $ pos1 `elem` psns) && (pos2 `elem` psns))
+    where
+        psns = map fst $ filter ((== target) . snd) $ zip [1..] pass
