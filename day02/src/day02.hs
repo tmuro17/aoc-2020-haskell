e79b6eb8a0aba1d7dd57a4aac48a97c83ffa373c
Tanner Muro <tmuro17@gmail.com>
2020-12-03 00:39:05 -0500

    Setup tests for day02

--- day02/src/day02.hs
+++ day02/src/day02.hs
@@ -2,14 +2,29 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 import qualified Data.Map as M
+import Test.Tasty
+import Test.Tasty.Hspec
 
 main :: IO ()
 main = do
-        text <- TIO.readFile "data/input2.txt"
-        lns <- pure $ T.lines text
-        print $ length $ filter (\[range, tc, pass] -> isValid (rangeToTuple range) (targetChar tc) (T.unpack pass)) $ map (T.words) lns
-        print $ length $ filter (\[range, tc, pass] -> isValid' (rangeToTuple range) (targetChar tc) (T.unpack pass)) $ map (T.words) lns
+    spc <- spec
+    tSpec <- testSpec "day2" spc
+    defaultMain (testGroup "Tests" [tSpec])
 
+spec :: IO (SpecWith ())
+spec = do
+    text <- TIO.readFile "data/input2.txt"
+    lns <- pure $ T.lines text
+    return $ context "spec" $ do
+        describe "overall" $ do
+            it "passes 2a" $
+                (length $ filter (\[range, tc, pass] -> isValid (rangeToTuple range) (targetChar tc) (T.unpack pass)) $ map (T.words) lns)
+                `shouldBe`
+                416
+            it "passes 2b" $
+                (length $ filter (\[range, tc, pass] -> isValid' (rangeToTuple range) (targetChar tc) (T.unpack pass)) $ map (T.words) lns)
+                `shouldBe`
+                 688
 
 rangeToTuple :: T.Text -> (Integer, Integer)
 rangeToTuple txt = (\[l, u] -> (read $ T.unpack l, read $ T.unpack u)) $ T.splitOn (T.pack "-") txt
