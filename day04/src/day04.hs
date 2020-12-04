77b7f6154ad4e74fc1e05bbc5828a4abd94f1770
Tanner Muro <tmuro17@gmail.com>
2020-12-04 01:26:05 -0500

    Day04 cleanup and testification

--- day04/src/day04.hs
+++ day04/src/day04.hs
@@ -1,22 +1,41 @@
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+
+-- Someone's library so that I can split lists easily
 import Data.List.Split (splitOn)
---import Test.Tasty
---import Test.Tasty.Hspec
+
+import Test.Tasty
+import Test.Tasty.Hspec
 
 main :: IO ()
 main = do
-    text <- TIO.readFile "data/input4.txt"
-    lines <- pure $ T.lines text
-    strs <- pure $ map T.unpack lines
-    split <- pure $ splitOn [""] strs
-    oneLines <- pure $ map unwords split
-    detailList <- pure $ map words oneLines
-    keyValTuples <- pure $ map (map keyPairTuple) detailList
-    keyLists <- pure $ map (map fst) keyValTuples
-    print $ length $ filter hasRequired keyLists
-    print $ length $ filter (all isValidEntry) $ filter (\x -> hasRequired (map fst x)) keyValTuples
+    spc <- spec
+    tSpec <- testSpec "day4" spc
+    defaultMain (testGroup "Tests" [tSpec])
 
+spec :: IO (SpecWith ())
+spec = do
+    text <- TIO.readFile "data/input4.txt"
+    lns <- pure $ T.lines text
+    strs <- pure $ map T.unpack lns
+    return $ context "spec" $ do
+        describe "overall" $ do
+            it "passes 4a" $
+                solve4a strs
+                `shouldBe`
+                208
+            it "passes 4b" $
+                solve4b strs
+                `shouldBe`
+                167
+
+
+--Fixme: Provide some clarity to function compositions, or make some of them functions
+solve4a :: [String] -> Int
+solve4a = length . filter hasRequired . map (map fst) . map (map keyPairTuple) . map words . map unwords . splitOn [""]
+
+solve4b :: [String] -> Int
+solve4b = length . filter (all isValidEntry) . filter (\x -> hasRequired (map fst x)) . map (map keyPairTuple) . map words . map unwords . splitOn [""]
 
 keyPairTuple :: String -> (String, String)
 keyPairTuple entry = (head splits, last splits)
@@ -42,34 +61,13 @@ isValidEntry ("hgt", hgt) | last hgt == 'm' = processCm num
                           | last hgt == 'n' = processIn num
                           | otherwise = False
     where
-        num = read (init $ init hgt)
+        num = read (init $ init hgt) :: Int
         processCm n = (n >= 150 && n <= 193)
         processIn n = (n >= 59 && n <= 76)
 isValidEntry ("hcl", color) = length color == 7 && isColor (tail color)
     where isColor c = all (\x -> (x `elem` ['0'..'9']) || (x `elem` ['a'..'f'])) c
 isValidEntry ("pid", num) = length num == 9 && all (`elem` ['0'..'9']) num
 isValidEntry ("cid", _) = True
+isValidEntry _ = False
 
--- Used to turn into Spec style later
-{-
-main :: IO ()
-main = do
-    spc <- spec
-    tSpec <- testSpec "day4" spc
-    defaultMain (testGroup "Tests" [tSpec])
 
-spec :: IO (SpecWith ())
-spec = do
-    text <- TIO.readFile "data/input4.txt"
-    lns <- pure $ T.lines text
-    strs <- pure $ map T.unpack lns
-    return $ context "spec" $ do
-        describe "overall" $ do
-            it "passes 4a" $
-                True
-                `shouldBe`
-                True
-            it "passes 4b" $
-                False
-                `shouldBe`
-                False-}
