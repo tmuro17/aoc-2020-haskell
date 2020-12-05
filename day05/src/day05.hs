4af0a86e92a070319b4698023d7643a0beb4ca32
Tanner Muro <tmuro17@gmail.com>
2020-12-05 00:58:32 -0500

    The point free makes some more clear and some less, but seeing as this repo is for me to have fun, I am keeping it

--- day05/src/day05.hs
+++ day05/src/day05.hs
@@ -1,25 +1,45 @@
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
--- Someone's library so that I can split lists easily
-import Data.List.Split (splitOn)
 import Control.Monad
 import Test.Tasty
 import Test.Tasty.Hspec
 
 main :: IO ()
 main = do
+    spc <- spec
+    tSpec <- testSpec "day5" spc
+    defaultMain (testGroup "Tests" [tSpec])
+
+spec :: IO (SpecWith ())
+spec = do
     text <- TIO.readFile "data/input5.txt"
     lns <- pure $ T.lines text
     strs <- pure $ map T.unpack lns
-    print $ maximum $ map seatID $ strs
-    there <- pure $ map seatID strs
-    print $ filter (flip plusMinus there) $ (missing strs)
+    return $ context "spec" $ do
+        describe "overall" $ do
+            it "passes 5a" $
+                solve5a strs
+                `shouldBe`
+                930
+            it "passes 5b" $
+                solve5b strs
+                `shouldBe`
+                515
 
-plusMinus seat there = (seat + 1 `elem` there) && (seat - 1 `elem` there)
+solve5a :: [String] -> Int
+solve5a = maximum . map seatID
 
-missing strs = (\there possible -> filter (not . (`elem` there)) possible) (map seatID strs) allSeats
+solve5b :: [String] -> Int
+solve5b = head . ap (filter . flip plusMinus . map seatID) missing
 
+plusMinus :: Int -> [Int] -> Bool
+plusMinus = ap (ap . ((&&) .) . elem . (1 +)) (elem . subtract 1)
+
+missing :: [String] -> [Int]
+missing = flip filter allSeats . (not .) . flip elem . map seatID
+
+allSeats :: [Int]
 allSeats = [x * 8 + y | x <- [0..127], y <- [0..7]]
 
 seatID :: String -> Int
@@ -31,24 +51,4 @@ rowNum = sum . map fst . filter ((=='B') . snd) . zip [2 ^ x | x <- [6,5..0]]
 colNum :: String -> Int
 colNum = sum . map fst . filter ((=='R') . snd) . zip [2 ^ x | x <- [2,1,0]]
 
---main :: IO ()
---main = do
---    spc <- spec
---    tSpec <- testSpec "day4" spc
---    defaultMain (testGroup "Tests" [tSpec])
---
---spec :: IO (SpecWith ())
---spec = do
---    text <- TIO.readFile "data/input4.txt"
---    lns <- pure $ T.lines text
---    strs <- pure $ map T.unpack lns
---    return $ context "spec" $ do
---        describe "overall" $ do
---            it "passes 4a" $
---                solve4a strs
---                `shouldBe`
---                208
---            it "passes 4b" $
---                solve4b strs
---                `shouldBe`
---                167
+
