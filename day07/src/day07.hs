e429b71e5f8bb2faf19ac977b20f955e6d23dde0
Tanner Muro <tmuro17@gmail.com>
2020-12-07 15:50:59 -0500

    Minor cleanup. From now on, when replacing  things with the pointfree version I will leave the older function commented out underneath.

--- day07/src/day07.hs
+++ day07/src/day07.hs
@@ -12,30 +12,49 @@ data Content = Content Bag Int deriving (Show, Eq, Ord)
 
 main :: IO ()
 main = do
+    spc <- spec
+    tSpec <- testSpec "day7" spc
+    defaultMain (testGroup "Tests" [tSpec])
+
+spec :: IO (SpecWith ())
+spec = do
     text <- TIO.readFile "data/input7.txt"
     lns <- pure $ T.lines text
     strs <- pure $ map T.unpack lns
-    ruleMap <- pure $ toRuleMap $ map toRule $ map words $ strs
-    candidates <- pure $ filter (/= "shiny gold") $ map (\(Rule bag _) -> bag) $ map toRule $ map words $ strs
-    print $ length $ filter (canContain ruleMap "shiny gold") $ candidates
-    print $ countInnerBags ruleMap "shiny gold"
-
-ex = ["shiny gold bags contain 2 dark red bags.",
-      "dark red bags contain 2 dark orange bags.",
-      "dark orange bags contain 2 dark yellow bags.",
-      "dark yellow bags contain 2 dark green bags.",
-      "dark green bags contain 2 dark blue bags.",
-      "dark blue bags contain 2 dark violet bags.",
-      "dark violet bags contain no other bags."]
-
+    return $ context "spec" $ do
+        describe "overall" $ do
+            it "passes 7a" $
+                solve7a strs
+                `shouldBe`
+                226
+            it "passes 7b" $
+                solve7b strs
+                `shouldBe`
+                9569
 
 type RuleMap = M.Map Bag [Content]
 
+solve7a = length . ap (filter . flip canContain "shiny gold" . toRuleMap . map (toRule . words)) (filter ("shiny gold" /=) . map (toBag . words))
+--solve7a strs = length $ filter (canContain ruleMap "shiny gold") $ candidates
+--    where
+--        ruleMap = toRuleMap $ map toRule $ map words $ strs
+--        candidates = filter (/= "shiny gold") $ map toBag $ map toRule $ map words $ strs
+
+
+solve7b = flip countInnerBags "shiny gold" . toRuleMap . map (toRule . words)
+
+--solve7b strs = countInnerBags ruleMap "shiny gold"
+--    where ruleMap = toRuleMap $ map toRule $ words strs
+
+toBag :: [String] -> Bag
+toBag = unwords . take 2
+
 toRule :: [String] -> BagRule
-toRule spec = Rule color contents
-    where
-        color = unwords $ take 2 spec
-        contents = toContents $ drop 4 spec
+toRule = liftM2 Rule toBag (toContents . drop 4)
+--toRule specs = Rule color contents
+--    where
+--        color = toBag specs
+--        contents = toContents $ drop 4 specs
 
 toContents :: [String] -> [Content]
 toContents ["bag."] = []
@@ -50,35 +69,14 @@ toRuleMap = M.fromList . map (\(Rule bag cnts) -> (bag, cnts))
 canContain :: M.Map Bag [Content] -> Bag -> Bag -> Bool
 canContain _ target search | target == search = True
 canContain ruleMap target search = case M.lookup search ruleMap of
-    Just contents ->  or $ map (\x -> canContain ruleMap target x) $ map (\(Content bag _) -> bag) $ contents
     Nothing -> False
+    Just contents ->  or $ map (canContain ruleMap target) $ map (\(Content bag _) -> bag) $ contents
 
 
 countInnerBags :: RuleMap -> Bag -> Int
 countInnerBags ruleMap key =  case M.lookup key ruleMap of
     Just [] -> 0
-    Just contents -> sum $ map (\(Content bag num) -> max (num + num * countInnerBags ruleMap bag) num) contents
     Nothing ->  0
+    Just contents -> sum $ map (\(Content bag num) -> max (num + num * countInnerBags ruleMap bag) num) contents
 
 
---main :: IO ()
---main = do
---    spc <- spec
---    tSpec <- testSpec "day6" spc
---    defaultMain (testGroup "Tests" [tSpec])
---
---spec :: IO (SpecWith ())
---spec = do
---    text <- TIO.readFile "data/input6.txt"
---    lns <- pure $ T.lines text
---    strs <- pure $ map T.unpack lns
---    return $ context "spec" $ do
---        describe "overall" $ do
---            it "passes 6a" $
---                solve6a strs
---                `shouldBe`
---                6686
---            it "passes 6b" $
---                solve6b strs
---                `shouldBe`
---                3476
